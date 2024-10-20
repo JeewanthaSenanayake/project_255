cca97917dfc076a867b1b22fcac32536ef6647f0
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-10-20 11:44:59 +0530

    election distric map intigrate

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:project_225/MapScreen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,124 +14,127 @@ class _LoginScreenState extends State<LoginScreen> {
     double scrnwidth = MediaQuery.of(context).size.width;
     double scrnheight = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SingleChildScrollView(
-        child: Container(
-          color: Colors.black,
-          height: scrnheight,
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/The_Parliament_Login_Screen.png",
-                width: scrnwidth,
-              ),
-              // Image.asset(
-              //   "assets/loading.gif",
-              //   width: scrnwidth * 0.2,
-              // ),
-              Container(
-                margin: EdgeInsets.all(scrnwidth * 0.025),
-                // color: Colors.white,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.all(scrnheight * 0.01),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Username",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                          ),
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/The_Parliament_Login_Screen.png",
+              width: scrnwidth,
+            ),
+            // Image.asset(
+            //   "assets/loading.gif",
+            //   width: scrnwidth * 0.2,
+            // ),
+            Container(
+              margin: EdgeInsets.all(scrnwidth * 0.025),
+              // color: Colors.white,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(scrnheight * 0.01),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Username",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                    Padding(
-                      padding: EdgeInsets.all(scrnheight * 0.01),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          hintStyle: TextStyle(
-                            color: Colors.white,
-                          ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(scrnheight * 0.01),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.white,
                         ),
                       ),
                     ),
-                    Container(
-                      alignment: Alignment.topRight,
-                      child: TextButton(
-                          onPressed: () {
-                            print("Forgot Password?");
-                          },
-                          child: Text(
-                            "Forgot Password?",
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: TextButton(
+                        onPressed: () {
+                          print("Forgot Password?");
+                        },
+                        child: Text(
+                          "Forgot Password?",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(scrnheight * 0.01),
+                    width: scrnwidth * 0.5,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MapScreen()),
+                        );
+                      },
+                      child: Text("Login"),
                     ),
-                    Container(
-                      padding: EdgeInsets.all(scrnheight * 0.01),
-                      width: scrnwidth * 0.5,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Text("Login"),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(scrnheight * 0.01),
+                    child: Text(
+                      "or continue with",
+                      style: TextStyle(
+                        color: Colors.amber,
                       ),
                     ),
-                    Container(
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      print("Google Sign In");
+                    },
+                    child: Container(
                       padding: EdgeInsets.all(scrnheight * 0.01),
-                      child: Text(
-                        "or continue with",
+                      child: Image.asset(
+                        "assets/google.png",
+                        width: scrnwidth * 0.1,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
                         style: TextStyle(
                           color: Colors.amber,
                         ),
                       ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        print("Google Sign In");
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(scrnheight * 0.01),
-                        child: Image.asset(
-                          "assets/google.png",
-                          width: scrnwidth * 0.1,
+                      TextButton(
+                        onPressed: () {
+                          print("Sign Up");
+                        },
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't have an account?",
-                          style: TextStyle(
-                            color: Colors.amber,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            print("Sign Up");
-                          },
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:project_225/LoginScreen.dart';
 import 'firebase_options.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  // Disable screen rotation
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
